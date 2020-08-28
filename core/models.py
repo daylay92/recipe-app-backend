51a442e4df06c14ebec962f6ca96ea4c05830347
Ayodele <daylay92@yahoo.com>
2020-08-28 01:39:37 +0100

    fix lint issues

--- core/models.py
+++ core/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    PermissionsMixin,
+    AbstractBaseUser,
+)
 
 # Create your models here.
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password, **kwargs):
         """ Creates a new user and saves to the db """
         user = self.model(email=email, **kwargs)
@@ -13,10 +16,11 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
     is_active = models.BooleanField(default=True)
-    is_staff =  models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     objects = UserManager()
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
